7bdb7f8e88a9508d259de98db4b8a1cf62b5c3d9
drivedemon <superdriveza@hotmail.com>
2019-06-13 16:07:34 +0700

    + Chapter 7

--- client/src/main.js
+++ client/src/main.js
@@ -8,8 +8,8 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  el: '#app', // ให้มองหา id=”app” ใน index.html แล้ว render
+  router, //  เชื่อมโยงกับ router ที่โหลดมา
+  components: { App }, // ใช้ component นี้ไป render
+  template: '<App/>' //  บอกว่า App คือ component
 })
